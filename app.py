d247478a765f84d4b17e1122782ef583e3ce2535
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2024-02-22 22:29:41 +0000

    Get weather forecast

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request
 
-from helpers import lookup
+from helpers import getWeatherForecast
 
 app = Flask(__name__)
 
@@ -8,3 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/forecast")
+def forecast():
+    if not request.args.get("latitude") or not request.args.get("longitude"):
+        return redirect("/")
+
+    forecast = getWeatherForecast(request.args.get("latitude"), request.args.get("longitude"))
+
+    if not forecast:
+        return redirect("/")
+
+    return render_template("index.html", forecast=forecast)
